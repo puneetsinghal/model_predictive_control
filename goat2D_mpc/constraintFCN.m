910ac6cf2208817b09c2a762ea7a08e4486d2954
LAPTOP-RKOQHJI7\Puneet <puneetatpks@gmail.com>
2017-11-05 15:08:58 -0500

    changing constraint function to have non linear constraint on states bound

--- goat2D_mpc/constraintFCN.m
+++ goat2D_mpc/constraintFCN.m
@@ -1,4 +1,4 @@
-function [c, ceq] = constraintFCN(u, x, xref, Ts, N)
+function [c, ceq] = constraintFCN(u, x, xref, Ts, N, global_link_length)
 %% Constraint function of nonlinear MPC for pendulum swing-up and balancing control
 %
 % Inputs:
@@ -15,25 +15,24 @@ function [c, ceq] = constraintFCN(u, x, xref, Ts, N)
 
 %% Nonlinear MPC design parameters
 % Range of cart position: from -10 to 10
-zMin = -10;
-zMax = 10;
+zMin = [0; 0; 0; pi/2; -pi/2; -pi/2];
+zMax = [pi/2; pi; pi/2; pi; 0; 0];
 
 %% Inequality constraints calculation
-c = zeros(N*2,2);
+c = zeros(12,N);
 % Apply 2*N cart position constraints across prediction horizon, from time
 % k+1 to k+N
 xk = x;
 uk = u(:,1);
 for ct=1:N
-    % obtain new cart position at next prediction step
+    xk1 = goatDynamicsDT(xk, uk, Ts, global_link_length);
+   
+    c(1:6,ct) = -[xk1(1:3);xk1(7:9)] + zMin;
+    c(7:12,ct) =  [xk1(1:3);xk1(7:9)] - zMax;
     
-    xk1 = goatDynamicsDT(xk, uk, Ts);
-    % -z + zMin < 0
-%     c(2*ct-1) = -xk1(1)+zMin;
-    % z - zMax < 0
-%     c(2*ct) = xk1(1)-zMax;
-    % update plant state and input for next step
+     % update plant state and input for next step
     xk = xk1;
+    
     if ct<N
         uk = u(:,ct+1);
     end
