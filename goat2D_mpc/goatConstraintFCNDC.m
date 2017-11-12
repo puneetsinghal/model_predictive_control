cc50a45ed584e0b4635b686df14fd60a91aa0f79
Akshay Kumar <33128582+Manbeast-95@users.noreply.github.com>
2017-11-11 21:23:22 -0500

    Updated with inequality constraints

--- goat2D_mpc/goatConstraintFCNDC.m
+++ goat2D_mpc/goatConstraintFCNDC.m
@@ -1,25 +1,38 @@
 function [c, ceq] = goatConstraintFCNDC(p, x, xref, Ts, N, link_length)
-%% Constraint function of nonlinear MPC for pendulum swing-up and balancing control
-%
-% Inputs:
-%   u:      optimization variable, from time k to time k+N-1 
-%   x:      current state at time k
-%   Ts:     controller sample time
-%   N:      prediction horizon
-%
-% Output:
-%   c:      inequality constraints applied across prediction horizon
-%   ceq:    equality constraints (empty)
-%
-% Copyright 2016 The MathWorks, Inc.
-
 %% Nonlinear MPC design parameters
-% Range of cart position: from -10 to 10
-zMin = [0; 0; 0; pi/2; -pi/2; -pi/2];
-zMax = [pi/2; pi; pi/2; pi; 0; 0];
+% Range of theta posn
+zMin = [0; 0; 0];
+zMax = [pi/2; pi; pi/2];
+c = [];
+%% Equality constraints
+%Constarian Initial Point
+ceq = p(1:6,1) - x;
+for tk = 1:N-1   
+    
+    xk = [p(1:6,tk)];
+    uk = p(7:8,tk);
+    xdotk = (goatDynamicsCT(xk,uk,link_length));
+    
+    xk1 = [p(1:6,tk+1)];
+    uk1 = p(7:8,tk+1);
+    xdotk1 = (goatDynamicsCT(xk1,uk1,link_length));
+    ineq = [-xk1(1:3)+zMin;xk1(1:3)-zMax];
+    c = [c ineq];
+    
+    xkc = (xk+xk1)/2 + Ts*(xdotk - xdotk1)/8;
+    ukc = (uk+uk1)/2;
+    xdotkc = (goatDynamicsCT(xkc,ukc,link_length));
+    %Defect
+    delk = (xk - xk1) + Ts*(xdotk+4*xdotkc+xdotk1)/6;
+    ceq = [ceq delk];
+end    
+    %Constrain Final point
+    ceq = [ceq [p(1:6,N)-xref]];
+    ceq = real(ceq);
+
+
 
 %% Inequality constraints calculation
-c = [];
 % c = zeros(12,N);
 % % Apply 2*N cart position constraints across prediction horizon, from time
 % % k+1 to k+N
@@ -40,27 +53,5 @@ c = [];
 %         uk = u(:,ct+1);
 %     end
 % end
-%% Equality constraints
-%Constarian Initial Point
-ceq = p(1:6,1) - x;
-for tk = 1:N-1   
-    
-    xk = [p(1:6,tk)];
-    uk = p(7:8,tk);
-    xdotk = goatDynamicsCT(xk,uk,link_length);
-    
-    xk1 = [p(1:6,tk+1)];
-    uk1 = p(7:8,tk+1);
-    xdotk1 = goatDynamicsCT(xk1,uk1,link_length);
-    
-    xkc = (xk+xk1)/2 + Ts*(xdotk - xdotk1)/8;
-    ukc = (uk+uk1)/2;
-    xdotkc = goatDynamicsCT(xkc,ukc,link_length);
-    %Defect
-    delk = (xk - xk1) + Ts*(xdotk+4*xdotkc+xdotk1)/6;
-    ceq = [ceq delk];
-end    
-    %Constrain Final point
-    ceq = [ceq [p(1:6,N)-xref]];
-    ceq = real(ceq);
+
 end
