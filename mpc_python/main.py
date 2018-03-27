be95f75b76183dabce7d58a1fe01c0345eac77ad
puneetsinghal <puneetatpks@gmail.com>
2018-03-26 22:13:05 -0400

    created class for MPC controller to make the impplementation modular

--- mpc_python/main.py
+++ mpc_python/main.py
@@ -1,74 +1,20 @@
-# Evan Harber eharber@andrew.cmu.edu
+# Author:
+#     Puneet Singhal: puneetatpks@gmail.com
+#     Aditya Raghu Chavvali:
+
 # For detailed information on the minimize function visit:
 # https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html
 
-from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 import numpy as np
 from math import *
 import time
-from robot import Acrobot
 import pickle
 import argparse
 from copy import copy
 
-
-print('Start Control')
-
-def IntegrationEstimation(xk, uk, robot, M = 5):
-    # Runge-Kutta 4th order (M = 5 optimization problem, M = 30 updating state space)
-    # Better ODE solvers can be used here 
-    delta = robot.Ts/M
-    xk1 = xk
-    for ct in range(M):
-        k1 = robot.dynamics(xk1, uk)
-        k2 = robot.dynamics(xk1 + k1*delta/2, uk)
-        k3 = robot.dynamics(xk1 + k2*delta/2, uk)
-        k4 = robot.dynamics(xk1 + k3*delta, uk)
-        xk1 = xk1 + delta*(k1/6 + k2/3 + k3/3 + k4/6)
-    return xk1
-
-def CostFunction(u, xk, u0, xref, robot):
-    # Cost function taken from MatLab's nMPC example code 
-    Q = np.diag([100.,10.,0.1, 0.1])
-    R = 0.01
-    J = 0.
-
-    for ct in range(len(u)):
-        uk = u[ct]
-
-        xk1 = IntegrationEstimation(xk, uk, robot);
-
-        # for i in range(len(xk1)):
-        J += np.matmul(np.matmul((xk1-xref).T,Q),(xk1-xref))
- 
-        if ct==0:
-            J += (uk-u0)*R*(uk-u0)
-        else:
-            J += (uk-u[ct-1])*R*(uk-u[ct-1])
-        
-        xk = copy(xk1)
-    return J
-
-def Contraints(u, xk, robot):
-    phiMin = -10
-    phiMax = 10
-
-    c = np.zeros(robot.N*2)
-
-    for ct in range(len(u)):
-        uk = u[ct]
-
-        xk1 = IntegrationEstimation(xk, uk, robot, 5)
-
-        # -phi + phiMin > 0
-        c[2*ct] = xk1[0]-phiMin
-        # phi - phiMax > 0
-        c[2*ct+1] = -xk1[0]+phiMax
-
-        #update plant state and input for next step
-        xk = xk1
-    return c
+from robot import Acrobot
+from mpc import MPC
 
 if __name__ == '__main__':
 
@@ -77,6 +23,8 @@ if __name__ == '__main__':
     parser.add_argument('--suffix', type=str, default='')
 
     args = parser.parse_args()
+    t = time.time()
+
 
     params = {}
     params['m1'] = 1.
@@ -86,9 +34,6 @@ if __name__ == '__main__':
     params['g'] = -9.81
     params['I1'] = 0.1
     params['I2'] = 0.1
-
-    t = time.time()
-
     params['x0'] = np.array([0, 0, 0, 0]).reshape((4,1)) # current state space
     params['Ts'] = 0.05             # time iteration
     params['N'] = 10              # Event horizon = 10
@@ -96,10 +41,7 @@ if __name__ == '__main__':
     params['Duration'] = 2.5        # max time 
     params['numIterations'] = int(params['Duration']/params['Ts'])
     print('total number of iterations are: {}'.format(params['numIterations']))
-    
-    # xref1 = np.array([-pi/2, 0, 0, 0]).reshape((4,1)) # reference state space
-    # xf = np.array([pi, 0, 0, 0]).reshape((4,1))
-    
+
     params['xMidPoints'] = np.array([[0, 0.7022, 0.5790, 0.0513, -1.0845, -1.0845, 1.3254, 2.1820, 2.6058, 3.1416], 
                             [0, -1.2763, -2.2228, -2.0331, 0.0265, 2.4347, 4.1818, 4.4895, 3.9700, 3.1416],
                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -111,23 +53,30 @@ if __name__ == '__main__':
     #                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
     
     params['numsegment'] = params['xMidPoints'].shape[1] - 1 # subtracting one to get segments
+    params['Q'] = np.diag([100.,10.,0.1, 0.1])
+    params['R'] = 0.01
 
     uopt = np.zeros(params['N']) 
     u0 = params['u0']
     x = params['x0']
+    
+    # arrays to save data
     uHistory = [u0]             # force history
     xHistory = [params['x0'][:,0].tolist()]       # position history
     xRefHistory = []
 
+    robot = Acrobot(params)
+    print("Robot object created")
+
     LB = -100            # input force Lower Bound 
     UP = 100             # input force Upper Bound
-
-    robot = Acrobot(params)
     bnds = ((LB, UP), (LB, UP), (LB, UP), (LB, UP), (LB, UP), (LB, UP), (LB, UP), (LB, UP), (LB, UP), (LB, UP))
-    cons = ({'type': 'ineq', 'fun': Contraints, 'args':(x, robot,)})
+    controller = MPC(robot, params, bnds)
+    print("Controller object created")
+
+    # cons = ({'type': 'ineq', 'fun': Contraints, 'args':(x, robot,)})
     # contraint jacobian can also be added to possibly speed up 
     
-
     if (args.mode == 'train'):
         for ct in range(params['numIterations']):
             print('iteration # {} of {}'.format(ct, params['numIterations']))
@@ -142,14 +91,14 @@ if __name__ == '__main__':
             print("reference value: {}, {}".format(index, xref.T))
 
             # optimize
-            res = minimize(CostFunction, uopt, args=(x, u0, xref, robot), method='SLSQP', bounds=bnds, constraints=cons)
-            # jac=func_deriv arg that could possibly speed up time
-            
+            results = controller.optimize(uopt, x, u0, xref)
+
             # prepare variable for next run and save the output in history arrays
-            uopt = res.x
+            uopt = results.x
             u0 = uopt[0]
-            x = IntegrationEstimation(x, u0, robot, 30)
+            x = controller.IntegrationEstimation(x, u0, 30)
             print("next state: {}".format(x.T))
+            
             uHistory.append(u0)
             xHistory += [x[:,0].tolist()]
 
---
+++ mpc_python/mpc.py
@@ -0,0 +1,85 @@
+
+from scipy.optimize import minimize
+# import numdifftools as nd
+import numpy as np
+from math import *
+from copy import copy
+
+
+class MPC(object):
+	"""docstring for MPC"""
+	def __init__(self, robot, params, bnds):
+		self.Q = params['Q']
+		self.R = params['R']
+		self.robot = robot
+		self.Ts = params['Ts']
+		self.N = params['N']
+		self.bnds = bnds
+		self.x = params['x0']
+		self.u0 = params['u0']
+		self.xref = params['x0']
+		self.cons = ({'type': 'ineq', 'fun': self.Contraints, 'args':()})
+		# self.jac = ({'type': 'ineq', 'fun': self.Jacobian, 'args':()})
+
+	def IntegrationEstimation(self, xk, uk, M = 5):
+		# Runge-Kutta 4th order (M = 5 optimization problem, M = 30 updating state space)
+		# Better ODE solvers can be used here 
+		delta = self.Ts/M
+		xk1 = xk
+		for ct in range(M):
+			k1 = self.robot.dynamics(xk1, uk)
+			k2 = self.robot.dynamics(xk1 + k1*delta/2, uk)
+			k3 = self.robot.dynamics(xk1 + k2*delta/2, uk)
+			k4 = self.robot.dynamics(xk1 + k3*delta, uk)
+			xk1 = xk1 + delta*(k1/6 + k2/3 + k3/3 + k4/6)
+		return xk1
+
+	def CostFunction(self, u):
+	    # Cost function taken from MatLab's nMPC example code 
+		J = 0.
+		xk = self.x
+
+		for ct in range(len(u)):
+			uk = u[ct]
+
+			xk1 = self.IntegrationEstimation(xk, uk);
+
+			# for i in range(len(xk1)):
+			J += np.matmul(np.matmul((xk1-self.xref).T,self.Q),(xk1-self.xref))
+
+			if ct==0:
+				J += (uk-self.u0)*self.R*(uk-self.u0)
+			else:
+				J += (uk-u[ct-1])*self.R*(uk-u[ct-1])
+
+			xk = copy(xk1)
+		return J
+
+	def Contraints(self, u):
+		phiMin = -10
+		phiMax = 10
+		xk = self.x
+		c = np.zeros(self.N*2)
+
+		for ct in range(len(u)):
+			uk = u[ct]
+
+			xk1 = self.IntegrationEstimation(xk, uk, 5)
+
+			# -phi + phiMin > 0
+			c[2*ct] = xk1[0]-phiMin
+			# phi - phiMax > 0
+			c[2*ct+1] = -xk1[0]+phiMax
+
+			#update plant state and input for next step
+			xk = xk1
+		return c
+
+	def Jacobian(self, u):
+		# return nd.jacobian(self.CostFunction(u))
+
+	def optimize(self, uopt, x, u0, xref):
+		self.x = x
+		self.u0 = u0
+		self.xref = xref
+		return minimize(self.CostFunction, uopt, args=(), method='SLSQP', bounds=self.bnds, constraints=self.cons)
