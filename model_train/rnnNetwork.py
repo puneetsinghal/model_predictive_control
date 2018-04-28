5a53875c57ab5f61f651b766d8a6d571c9c15194
weilunwc <weilunwc@andrew.cmu.edu>
2018-04-28 19:07:31 -0400

    rnn

--- model_train/rnnNetwork.py
+++ model_train/rnnNetwork.py
@@ -40,26 +40,27 @@ class RNNNetwork():
         self.output_state_size = outout_state_size
         # Build network
         # Inputs
+        # Current state x_t
         self.current_state = tf.placheholder(tf.float32, [None, None, input_state_size])
         self.prev_state = tf.placeholder(tf.float32, [None, None, input_state_size])
         self.future_state = tf.placeholder(tf.float32, [None, None, output_state_size])
         self.network_batch_size = tf.placeholder(tf.int32, [])
         
+        # LSTM cell
         cell = tf.contrib.rnn.LSTMCell(hidden_state_size, state_is_tuple=True)
         init_states = cell.zero_state(network_batch_size, tf.float32)
         rnn_outputs, final_state = tf.nn.dynamic_rnn(cell, self.prev_state, initial_state=init_states)
         outputs = tf.reshape(rnn_outputs, [-1, hidden_state_size])
         
-       
-
+        # Combine LSTM output with control usage and current state
         W1 = tf.get_variable('W1', [hidden_state_size, output_state_size])
         b1 = tf.get_variable('b1', [output_state_size])
         
         state = tf.reshape(self.current_state, [-1, input_state_size])
         W2 = tf.get_variable('W2', [input_state_size, output_state_size])
         b2 = tf.get_variable('b2', [output_state_size])
-        
         predictions = (tf.matmul(outputs, W1) + b1) + (tf.matmul(state, W2) + b2)
+
         self.loss = tf.losses.mean_squared_error(
                 predictions, tf.reshape(self.future_state, [-1, output_state_size]))
        
@@ -139,7 +140,10 @@ def main():
         sess.run(tf.global_variables_initializer())
         for epoch in range(num_epoch):
             for batch in range(num_batch):
-                train_loss, _  = sess.run([loss, train_step], feed_dict={network_batch_size:batch_size,rnn_inputs:input_epoch[batch], actual_state:output_epoch[batch]})
+                train_loss, _  = sess.run([loss, train_step], feed_dict={ \
+                        network_batch_size:batch_size, \
+                        rnn_inputs:input_epoch[batch], \
+                        actual_state:output_epoch[batch]})
             if epoch % 100 == 0:
                 print("epoch", epoch, "loss", train_loss)
 
